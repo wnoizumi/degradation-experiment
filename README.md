5ba861ad0a919cf734c057d07868cf53c6afb7dc
Willian Oizumi <oizumi.willian@gmail.com>
2020-11-26 00:17:17 -0300

    fixing types of smells for each pattern

--- README.md
+++ README.md
@@ -5,16 +5,16 @@ Software system designed for running an experiment about code smells and maintai
 
 # Smell Patterns 
 
-Concern Overload - Complex Class, Feature Envy, God Class, Intensive Coupling, Long Method, and Shotgun Surgery
+Concern Overload - Complex Class, Feature Envy, God Class, Spaghetti Code, Brain Class, Intensive Coupling, Long Method, Brain Method, and Shotgun Surgery
 
 Fat Interface - (Shotgun Surgery in the interface) or (Dispersed Coupling, and Feature Envy in elements that are clients or implement the interface)
 
 Incomplete Abstraction - Lazy Class
 
-Scattered Concern - (God Class or Complex Class) + (Dispersed Coupling, Feature Envy, Intensive Coupling, and Shotgun Surgery)
+Scattered Concern - (God Class or Spaghetti Code or Brain Class or Complex Class) + (Dispersed Coupling, Feature Envy, Intensive Coupling, and Shotgun Surgery)
 
 Unused Abstraction - Speculative Generality
 
-Unwanted Dependency - Feature Envy, Long Method, and Shotgun Surgery
+Unwanted Dependency - Feature Envy, Long Method or Brain Method, and Shotgun Surgery
 
 
--- src/br/pucrio/opus/smells/patterns/model/SmellsOfPatterns.java
+++ src/br/pucrio/opus/smells/patterns/model/SmellsOfPatterns.java
@@ -8,11 +8,11 @@ import br.pucrio.opus.smells.collector.SmellName;
 public class SmellsOfPatterns {
 
 	//Multiple Smells Patterns
-	public static final List<SmellName> CONCERN_OVERLOAD = Arrays.asList(SmellName.ComplexClass, SmellName.FeatureEnvy, SmellName.GodClass, SmellName.IntensiveCoupling, SmellName.LongMethod, SmellName.ShotgunSurgery);
+	public static final List<SmellName> CONCERN_OVERLOAD = Arrays.asList(SmellName.BrainClass, SmellName.SpaghettiCode, SmellName.ComplexClass, SmellName.FeatureEnvy, SmellName.GodClass, SmellName.IntensiveCoupling, SmellName.LongMethod, SmellName.ShotgunSurgery);
 	public static final List<SmellName> FAT_INTERFACE = Arrays.asList(SmellName.DispersedCoupling, SmellName.FeatureEnvy);
 	public static final List<SmellName> UNWANTED_DEPENDENCY = Arrays.asList(SmellName.FeatureEnvy, SmellName.LongMethod, SmellName.ShotgunSurgery);
 	public static final List<SmellName> SCATTERED_CONCERN_COMPLEMENT = Arrays.asList(SmellName.DispersedCoupling, SmellName.FeatureEnvy, SmellName.IntensiveCoupling, SmellName.ShotgunSurgery);
-	public static final List<SmellName> SCATTERED_CONCERN_MANDATORY = Arrays.asList(SmellName.GodClass, SmellName.ComplexClass);
+	public static final List<SmellName> SCATTERED_CONCERN_MANDATORY = Arrays.asList(SmellName.GodClass, SmellName.ComplexClass, SmellName.BrainClass, SmellName.SpaghettiCode);
 	
 	//Single Smell Patterns
 	public static final List<SmellName> INCOMPLETE_ABSTRACTION = Arrays.asList(SmellName.LazyClass);
